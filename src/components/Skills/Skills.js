d54dda60e9dbc8c169bd06e008740c6a2762b027
Anaz N <anazbinnoushad@gmail.com>
2022-02-17 15:13:38 +0530

    skill page styling finshed on first row

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -4,25 +4,25 @@ import React from 'react'
 import './Skills.css'
 function Skills() {
   return (
-    <div>
+    <div className='skills'>
         <div class="skills_title_body">
             <h1 class="skills_title">SKILLS</h1>
         </div>
         <div className="html_title">
-            <p>HTML5</p>
-            <FontAwesomeIcon icon={faHtml5} />
+            <p className='skill_name'>HTML5</p>
+            <FontAwesomeIcon icon={faHtml5} className="icon"/>
         </div>
         <div className="css_title">
-            <p>CSS3</p>
-            <FontAwesomeIcon icon={faCss3Alt} />
+            <p className='skill_name'>CSS3</p>
+            <FontAwesomeIcon icon={faCss3Alt} className="icon"/>
         </div>
         <div className="javascript_title">
-            <p>JavaScript</p>
-            <FontAwesomeIcon icon={ faJsSquare } />
+            <p className='skill_name'>JavaScript</p>
+            <FontAwesomeIcon icon={ faJsSquare } className="icon"/>
         </div>
         <div className="react_title">
-            <p>React</p>
-            <FontAwesomeIcon icon={ faReact } />
+            <p className='skill_name'>React</p>
+            <FontAwesomeIcon icon={ faReact } className="icon"/>
         </div>
     </div>
 
