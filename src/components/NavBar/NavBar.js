ff688f27018b42f06072759b26276e4fa232f296
Anaz N <anazbinnoushad@gmail.com>
2022-03-12 20:27:38 +0530

    Navbar animated

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import gsap from 'gsap';
+import React ,{useRef , useEffect } from 'react'
 import './NavBar.css'
 
 function NavBar() {
+  const navRef = useRef()
+  const headTitleRef = useRef()
+  useEffect(()=>{
+    gsap.fromTo(navRef.current , { x : "200" }, {x : "0" , duration : 1.5})
+    gsap.fromTo(headTitleRef.current , { y : "-50" } , { y  : "0" , duration : 1.5})
+  },[]);
   return (
     <div className='title_navigation_bar'>
         <div className="navigation_bar_logo">
-                <h4 className="nav_logo"> hello </h4>
+                <h4 className="nav_logo" ref={headTitleRef}> hello </h4>
             </div>
-            <div className="navigation_bar_navs">
+            <div className="navigation_bar_navs" ref={navRef}>
                 <a href="#about">About</a>
                 <a href="#skills">Skills</a>
                 <a href="#contact">Contact</a>
