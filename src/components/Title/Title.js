39da91a30d2f41d28d71e7dec82560b3b519d93f
Anaz N <anazbinnoushad@gmail.com>
2022-03-13 21:47:09 +0530

    Section One Animated

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import gsap from 'gsap'
+import React , {useRef , useEffect} from 'react'
 import './Title.css'
 
 function Title() {
+  const titleRef = useRef()
+  const tagRef  = useRef()
+  useEffect(()=>{
+    gsap.fromTo(titleRef.current,{scale : 0},{ scale : 6 , ease : "elastic.out(1,0.3)" , duration : 2})
+    gsap.fromTo(tagRef.current,{ opacity : 0 , y : 200 },{ opacity : 1 , y : 0 , duration : 1})
+  },[])
   return (
     <div>
         <div className="the_title">
             <div className="title_name_container">
-                <h1 className="title_name"> Anaz N </h1>
+                <h1 className="title_name" ref={titleRef}> Anaz N </h1>
             </div>
             <div className="title_tag_container">
-                <p className="title_tag">Front-end Developer | React Js</p>
+                <p className="title_tag" ref={tagRef}>Front-end Developer | React Js</p>
             </div>
         </div>
     </div>
