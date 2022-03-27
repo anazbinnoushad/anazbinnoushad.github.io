979e49385195a287d49dc398dd1abea597aac456
Anaz N <anazbinnoushad@gmail.com>
2022-03-27 13:15:31 +0530

    Test deploy

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,6 +5,7 @@ import './NavBar.css'
 function NavBar() {
   const navRef = useRef()
   const headTitleRef = useRef()
+  const titleH= "<hello>"
   useEffect(()=>{
     gsap.fromTo(navRef.current , { x : "200" }, {x : "0" , duration : 1.5})
     gsap.fromTo(headTitleRef.current , { y : "-50" } , { y  : "0" , duration : 1.5})
@@ -12,7 +13,7 @@ function NavBar() {
   return (
     <div className='title_navigation_bar'>
         <div className="navigation_bar_logo">
-                <h4 className="nav_logo" ref={headTitleRef}> hello </h4>
+                <h4 className="nav_logo" ref={headTitleRef}> {titleH} </h4>
             </div>
             <div className="navigation_bar_navs" ref={navRef}>
                 <a href="#about">About</a>
